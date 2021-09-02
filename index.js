0dd93da475c8f724735d7408ae9c08383c457dcb
sahilnetic <jsthil99@gmail.com>
2021-09-02 20:08:35 +0530

    added path.resolve

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require('path')
 app.use(express.static('./public'))
 
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
 })
 
 
