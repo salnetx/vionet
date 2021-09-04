93953c0f3bfdd08feafaf30954a80b619156ea76
sahilnetic <jsthil99@gmail.com>
2021-09-04 17:29:05 +0530

    added everything

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
+app.get('/comingsoon',(req,res)=>{
+    res.sendFile(path.join(__dirname, 'public', 'comingsoon.html'))
+})
 
 
 app.listen(PORT,()=>{
