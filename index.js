f6d1f11e06a0008a7046cecef4bfdad64068bff6
sahilnetic <jsthil99@gmail.com>
2021-09-12 16:42:08 +0530

    almost done

--- index.js
+++ index.js
@@ -7,11 +7,21 @@ app.use(express.static('public'))
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
-
-app.get('/comingsoon',(req,res)=>{
-    res.sendFile(path.join(__dirname, 'public', 'comingsoon.html'))
+app.get('/apps',(req,res)=>{
+    res.sendFile(path.join(__dirname, 'public', 'app.html'))
 })
 
+app.get('/careers',(req,res)=>{
+    res.sendFile(path.join(__dirname, 'public', 'careers.html'))
+})
+
+app.get('/press',(req,res)=>{
+    res.sendFile(path.join(__dirname, 'public', 'press.html'))
+})
+
+app.get('/opensource',(req,res)=>{
+    res.sendFile(path.join(__dirname, 'public', 'opensource.html'))
+})
 
 app.listen(PORT,()=>{
     console.log(`App is booming at port ${PORT}`)
