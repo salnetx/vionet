2d06d005322a5a63b62388de59a702f79a11fc72
sahilnetic <jsthil99@gmail.com>
2021-09-30 14:48:00 +0530

    done as fuck

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 const path = require('path')
 // app.use(express.static('public'))
 
-app.get('/home',(req,res)=>{
+app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 app.get('/apps',(req,res)=>{
