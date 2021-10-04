22f8bca733aa577f5fc27e869601813dcde46129
sahilnetic <jsthil99@gmail.com>
2021-10-04 23:28:13 +0530

    created index.ejs

--- index.js
+++ index.js
@@ -1,27 +1,12 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
-const path = require('path')
-// app.use(express.static('public'))
+app.set('view engine','ejs')
 
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
-})
-app.get('/apps',(req,res)=>{
-    res.sendFile(path.join(__dirname, 'public', 'app.html'))
+    res.render('index')
 })
 
-app.get('/careers',(req,res)=>{
-    res.sendFile(path.join(__dirname, 'public', 'careers.html'))
-})
-
-app.get('/press',(req,res)=>{
-    res.sendFile(path.join(__dirname, 'public', 'press.html'))
-})
-
-app.get('/opensource',(req,res)=>{
-    res.sendFile(path.join(__dirname, 'public', 'opensource.html'))
-})
 
 app.listen(PORT,()=>{
     console.log(`App is booming at port ${PORT}`)
