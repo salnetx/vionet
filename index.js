7da3b67306b1e19482ca8306e1f779611eb97ecd
sahilnetic <jsthil99@gmail.com>
2021-10-10 23:05:10 +0530

    done

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const path = require('path')
 app.set('view engine','ejs')
-app.set('views', path.resolve(__dirname) + '/api/templates')
+app.set('views', path.resolve(__dirname) + '/api/templates');
+app.use('/static', express.static(path.join(__dirname, 'public')))
 
 app.get('/',(req,res)=>{
     res.render('index')
