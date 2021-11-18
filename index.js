3e110c99c51f3567a4ad5b26934b43a047fe0b8a
ericnetic <jsthil99@gmail.com>
2021-11-19 04:16:12 +0530

    done

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const nodemailer = require("nodemailer");
 const PORT = process.env.PORT || 5000;
 const path = require('path')
 app.set('view engine','ejs')
 app.set('views', path.resolve(__dirname) + '/api/templates');
 app.use('/static', express.static(path.join(__dirname, 'public')))
+
 
 app.get('/',(req,res)=>{
     res.render('index')
